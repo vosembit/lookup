574b3adcef63d3fb1d9d0772c01cf4f767e244cd
daladno13 <slickindub@gmail.com>
2016-10-30 07:04:42 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
 var canvas, ax, ay, bx, by, t;
+
+var spacer = 50;
+var side = 562;
+
 var inres = 256;    // Input Resolution
 var instep = 32;    // Input Mesh Step
 var infactor = 1;   // Input Multiple Factor
+
 var outres = 256;   // Output Resolution
 var outstep = 32;   // Output Mesh Step
 var outfactor = 1;  // Output Multiple Factor
+  
 var state = false;  // Print Calculations to the page
 var message;        // Save Calculations to the array
 
@@ -93,70 +99,69 @@ function showData() {
 }
 
 function draw() {
-
+  
   background('#333333');
-  noFill();
-
-  inputs();         //getting data from user/mouse
-  mesh();           //drawing background design based on selected resolution
-  myCurve();        //drawing main curve based on input data
-  controlPoints();  //drawing control points for adjusting curve 
-  points();         //calculations for Y-point based on selected resolution
+  
+  myMesh();         //  drawing background design based on selected resolution  
+  myCurve();        //  drawing main curve based on input data
+  mouseEvents();    //  getting data from user/mouse
+  calcs();          //  calculations for Y-point based on selected resolution
 }
 
 //background mesh design
-function mesh() {
-  strokeWeight(1);
-  stroke('#4D4D4D')
+function myMesh() {
   noFill();
-  rect(width / 2, height / 2, 512, 512);
-  for (var i = 50; i < 562; i += outstep) {
-    fill(96);
-    line(i, 50, i, height - 50);
-  }
-  for (var i = 50; i < 562; i += instep) {
-    fill(96);
-    line(50, i, height - 50, i);
-  }
-  for (var i = 50; i <= 562; i += instep * 4) {
-    noStroke();
-    fill(150);
-    textSize(8);
-    text((i - 50) / 2 * infactor, 30, height - i);
-  }
-  stroke(104);
-  line(306, 50, 306, height - 50);          // linear defaults
-  line(50, 306, width - 50, 306);           // middle line horizontal
-  line(50, height - 50, width - 50, 50);    // middle line vertical
   stroke('#4D4D4D')
-  for (var i = 50; i <= 562; i += outstep * 4) {
+  rect(width / 2, height / 2, 512, 512);
+  
+  for (var i = spacer; i < side; i += outstep) {
+    line(i, spacer, i, side);
+  }
+  for (var i = spacer; i < side; i += instep) {
+    line(spacer, i, side, i);
+  }
+  for (var i = spacer; i <= side; i += instep * 4) {
     noStroke();
     fill(150);
     textSize(8);
-    text((i - 50) / 2 * outfactor, i, height - 30);
+    text((i - spacer) / 2 * infactor, 30, height - i);
+  }
+  
+  stroke(104);
+  line(306, spacer, 306, side);           // linear defaults
+  line(spacer, 306, side, 306);           // middle line horizontal
+  line(spacer, side, side, spacer);       // middle line vertical
+
+  for (var i = spacer; i <= side; i += outstep * 4) {
+    noStroke();
+    fill(150);
+    textSize(8);
+    text((i - spacer) / 2 * outfactor, i, height - 30);
   }
 }
+
 //points on curve calcualtons. 
-function points() {
-  var flag = 0;
+function calcs() {
+
   // drawing 3 control point just for fun
   for (var i = 0; i <= 4; i++) {
     t = i / 4;
-    x = bezierPoint(50, ax, bx, width - 50, t);
-    y = bezierPoint(height - 50, ay, by, 50, t);
-    var value = floor((height - 50 - round(y)) / 2);
+    x = bezierPoint(spacer, ax, bx, side, t);
+    y = bezierPoint(side, ay, by, spacer, t);
+    var value = floor((side - y) / 2);
     fill(255);
     ellipse(x, y, 3, 3);
     textSize(8);
     text(value * infactor, x + 5, y + 5);
   }
+  
   // calculating point according to Resolution
   var values = [];
-  for (var i = 0; i <= outres; i++) {
+  for (var i = 0; i < outres; i++) {
     t = i / outres;
-    x = bezierPoint(50, ax, bx, width - 50, t);
-    y = bezierPoint(height - 50, ay, by, 50, t);
-    var value = floor((height - 50 - round(y)) / 2 * infactor);
+    x = bezierPoint(spacer, ax, bx, side, t);
+    y = bezierPoint(side, ay, by, spacer, t);
+    var value = floor((side - y) / 2 * infactor);
     append(values, value + ", ");
   }
   var separator = " "
@@ -167,50 +172,49 @@ function points() {
     state = false;
   }
 }
+
 //mouse handler
-function inputs() {
+function mouseEvents() {
   if (mouseIsPressed) {
     if (dist(mouseX, mouseY, ax, ay) < 50) {
       ax = mouseX;
       ay = mouseY;
-      if (mouseX >= 562) ax = 562;
-      if (mouseX <= 50) ax = 50;
-      if (mouseY >= 562) ay = 562;
-      if (mouseY <= 50) ay = 50;
+      if (mouseX >= side  ) ax = side;
+      if (mouseX <= spacer) ax = spacer;
+      if (mouseY >= side  ) ay = side;
+      if (mouseY <= spacer) ay = spacer;
     }
     if (dist(mouseX, mouseY, bx, by) < 50) {
       bx = mouseX;
       by = mouseY;
-      if (mouseX >= 562) bx = 562;
-      if (mouseX <= 50) bx = 50;
-      if (mouseY >= 562) by = 562;
-      if (mouseY <= 50) by = 50;
+      if (mouseX >= side  ) bx = side;
+      if (mouseX <= spacer) bx = spacer;
+      if (mouseY >= side  ) by = side;
+      if (mouseY <= spacer) by = spacer;
     }
   }
 }
-//control points design
-function controlPoints() {
-  strokeWeight(1);
-  stroke(255, 102, 0);
-  fill(255, 102, 0);
-  line(50, height - 50, ax, ay);
-  line(width - 50, 50, bx, by);
-  rect(ax, ay, 5, 5);
-  rect(bx, by, 5, 5);
-  fill(196);
-  noStroke();
-}
-//curve render based on control points
+
 function myCurve() {
   noFill();
   strokeWeight(1);
   stroke(255);
-  bezier(50, height - 50, ax, ay, bx, by, width - 50, 50);
+  //curve render based on control points
+  bezier(spacer, side, ax, ay, bx, by, side, spacer);
   beginShape();
   fill(127, 50);
   noStroke();
-  vertex(50, height - 50);
-  bezierVertex(ax, ay, bx, by, width - 50, 50);
-  bezierVertex(width - 50, 50, width - 50, height - 50, width - 50, height - 50);
-  endShape();;
+  vertex(spacer, side);
+  bezierVertex(ax, ay, bx, by, side, spacer);
+  bezierVertex(side, spacer, side, side, side, side);
+  endShape();
+  //drawing control points for adjusting curve 
+  stroke(255, 102, 0);
+  fill(255, 102, 0);
+  line(spacer, side, ax, ay);
+  line(side, spacer, bx, by);
+  rect(ax, ay, 5, 5);
+  rect(bx, by, 5, 5);
+  fill(196);
+  noStroke();
 }
