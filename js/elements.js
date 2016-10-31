789494a2536216802cca3f21773c7c431edbe199
daladno13 <slickindub@gmail.com>
2016-10-31 09:09:57 +0300

    Update elements.js

--- js/elements.js
+++ js/elements.js
@@ -1,63 +1,68 @@
 function elements() {
 
+  // HEADER AND PROJECT INFO
+  head = createElement('h1','LOOKUP TABLE GENERATOR');
+  head.position(50, 0);
+  head.id("head");
+  
   // INPUT RESOLUTION SELECTOR
   input = createP('INPUT:');
-  input.position(50, 57);
+  input.position(50, 77);
   input.id("input");
 
   // create input 8 bit resolution button
   input8 = createButton('256');
-  input8.position(100, 60);
+  input8.position(100, 80);
   input8.id("res");
   input8.mousePressed(in256);
 
   // create input 10 bit resolution button
   input10 = createButton('1024');
-  input10.position(150, 60);
+  input10.position(150, 80);
   input10.id("res");
   input10.mousePressed(in1024);
 
   // OUTPUT RESOLUTION SELECTOR
   output = createP('OUTPUT:');
-  output.position(226, 57);
+  output.position(226, 77);
   output.id("input");
 
   // create output 8 bit resolution button
   output8 = createButton('256');
-  output8.position(290, 60);
+  output8.position(290, 80);
   output8.id("res");
   output8.mousePressed(out256);
 
   // create output 10 bit resolution button
   output10 = createButton('1024');
-  output10.position(340, 60);
+  output10.position(340, 80);
   output10.id("res");
   output10.mousePressed(out1024);
 
   // create get button
   getData = createButton('RESET');
-  getData.position(450, 60);
+  getData.position(450, 80);
   getData.id("btn");
   getData.mousePressed(reset);
 
   // create canvas for curve and infografics
   canvas = createCanvas(612, 612);
-  canvas.position(0, 70);
+  canvas.position(0, 90);
   canvas.parent('curve');
 
   // create header text for calculated data
   summ = createElement('H3', summ);
-  summ.position(50, 650);
+  summ.position(50, 670);
   summ.id("summ");
 
   // INPUT RESOLUTION SELECTOR
   input = createP('(copy generated values to clipboard and paste it to your code)');
-  input.position(50, 680);
+  input.position(50, 700);
   input.id("input");
 
   // create paragraph text with calculated data
   result = createP(message);
-  result.position(50, 720);
+  result.position(50, 740);
   result.id("result");
 }
 
